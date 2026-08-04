3b59378cd1c362ee36bdadfaa41a7a7f14095dbd
umasankarmaity92-lab <umasankar.maity92@gmail.com>
2026-08-05 00:23:32 +0530

    gh

---
+++ .wrangler/tmp/pages-aGsi0A/functionsRoutes-0.22699450271561417.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ___middleware_js_onRequest } from "C:\\Users\\user\\Documents\\GitHub\\electromechcalc\\functions\\_middleware.js"
+
+export const routes = [
+    {
+      routePath: "/",
+      mountPath: "/",
+      method: "",
+      middlewares: [___middleware_js_onRequest],
+      modules: [],
+    },
+  ]
